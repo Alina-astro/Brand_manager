1a0a04291212a46ce258bb9fc260340fea37b47a
Alina Nadtochiy <am0817@mail.ru>
2025-03-06 16:50:58 +0300

    create component welcome

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,10 +1,18 @@
 import "./welcome.css";
+import arrowRightIcon from '../../assets/icons/arrow-right.png'
 
 function Welcome() {
     return (
-        <div className="welcome">
-            <h2>welcome</h2>
-        </div>
+        <section className="welcome">
+            <div className="welcome-content">
+                <p className="welcome-text">Создание и развитие вашего уникального имиджа — это искусство, которое я помогу вам освоить.
+                </p>
+                <p className="welcome-text">Сочетая опыт в люксовой индустрии и глубокое понимание персонального брендинга, я помогу вам уверенно представлять себя в деловой и социальной среде, учитывая все тонкости культурных различий и делового этикета.
+                </p>
+                <a href="#services" className="button">Подробнее об услугах <img src={arrowRightIcon} className="arrow" alt="arrow right" />
+                </a>
+            </div>
+        </section>
     );
 }
 
