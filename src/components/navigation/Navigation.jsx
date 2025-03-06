e431514267a00d0f1a944489ef0562684ccf01be
Alina Nadtochiy <am0817@mail.ru>
2025-03-06 03:28:07 +0300

    create component navigation

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,18 +1,22 @@
 import "./navigation.css";
+import emailIconBase from '../../assets/icons/nav-email-base.png';
+import whatsappIconBase from '../../assets/icons/nav-whatsapp-base.png';
+import telegramIconBase from '../../assets/icons/nav-telegram-base.png';
 
 function Navigation() {
     return (
         <div className="navigation">
+            <div className="nav-menu"></div>
             <h1>ALINA POPOVA</h1>
-            <div className="icons">
-            <a href="mailto:someone@example.com" className="icon">
-                <img src="#" alt="Email" />
+            <div className="nav-icons">
+            <a href="mailto:someone@example.com" className="icon emailIcon">
+                <img src={emailIconBase} alt="Email" />
             </a>
-            <a href="https://wa.me/1234567890" className="icon">
-                <img src="#" alt="WhatsApp" />
+            <a href="https://wa.me/1234567890" className="icon whatsappIcon">
+                <img src={whatsappIconBase} alt="WhatsApp" />
             </a>
-            <a href="https://t.me/username" className="icon">
-                <img src="telegram-icon.png" alt="Telegram" />
+            <a href="https://t.me/username" className="icon telegramIcon">
+                <img src={telegramIconBase} alt="Telegram" />
             </a>
             </div>
         </div>
