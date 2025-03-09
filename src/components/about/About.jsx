98baeb37a9e8a0f23554c50491bd21ba50dfa4e6
Alina Nadtochiy <am0817@mail.ru>
2025-03-09 23:44:13 +0300

    create component about (without styles)

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,37 @@
 import "./about.css";
+import aboutImage from "../../assets/images/welcome/about-photo.jpg";
 
-function About() {
+const About = () => {
     return (
-        <div className="about">
-            <h2>Обо мне</h2>
+      <section className="about">
+        <h2 className="about-title">Обо мне</h2>
+        <div className="about-content">
+          <div className="about-image-container">
+            <img src={aboutImage} alt="Алина Попова" className="about-image" />
+          </div>
+          <div className="about-text">
+            <div className="about-mission">
+                <p>
+                Моя миссия — создать для вас гармоничный, уверенный и запоминающийся образ, отражающий ваши уникальные черты и профессиональные цели. 
+                </p>
+                <p>
+                Мой подход основан на индивидуальности, знании мировых трендов и опыте работы с международными брендами.
+                </p>
+            </div>
+            <div className="about-career">
+                <p>
+                Меня зовут Алина Попова, я консультант по имиджу и личному бренду с более чем 10-летним опытом работы. 
+                </p>
+                <p>Получив образование в области Luxury and Fashion Brand Management в университете Боккони, я строила карьеру в люксовом сегменте в международных компаниях, таких как The Luxury Collection и Four Seasons.
+                </p>
+            </div>
+          </div>
         </div>
+        <div className="about-slogan">
+        Моя цель — помочь вам раскрыть свой потенциал через продуманный имидж и уверенное позиционирование.
+        </div>
+      </section>
     );
-}
-
-export default About;
+  };
+  
+  export default About;
