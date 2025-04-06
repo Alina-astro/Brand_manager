c5905ff23d8cbdb040aff0cd7417d051dcacbd76
Alina Nadtochiy <am0817@mail.ru>
2025-04-06 21:42:13 +0300

    add component faq without styles

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -1,10 +1,44 @@
+import { useState } from 'react';
 import "./faq.css";
+import arrowDown from "../../assets/icons/arrow-down.png";
+import arrowUp from "../../assets/icons/arrow-up.png";
+
+const faqData = [
+    { question: 'Как проходит консультация?', answer: 'Как проходит консультация?' },
+    { question: 'Вы работаете только с клиентами из бизнеса или и с частными лицами?', answer: 'Вы работаете только с клиентами из бизнеса или и с частными лицами?' },
+    { question: 'Могу ли я получить презентацию с рекомендациями после консультации?', answer: 'Могу ли я получить презентацию с рекомендациями после консультации?' },
+    { question: 'Предлагаете ли вы помощь в создании имиджа для команды компании?', answer: 'Предлагаете ли вы помощь в создании имиджа для команды компании?' },
+    { question: 'Какие документы или информация понадобятся перед началом работы?', answer: 'Какие документы или информация понадобятся перед началом работы?' },
+  ];
 
 function Faq() {
+    const [openIndex, setOpenIndex] = useState(null);
+
+  const toggle = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
     return (
-        <div className="faq">
+        <section className="faq">
             <h2>Популярные вопросы</h2>
-        </div>
+            <div className="faq-box">
+            {faqData.map((item, index) => (
+                    <div
+                    key={index}
+                    className={`faq-item ${openIndex === index ? 'open' : ''}`}
+                    onClick={() => toggle(index)}
+                    >
+                    <div className="faq-question">
+                        <span className="block-desc">{item.question}</span>
+                        <span className="arrow">
+                            <img src={openIndex === index ? arrowUp : arrowDown} alt="arrow" />
+                        </span>
+                    </div>
+                    <div className="faq-answer">{item.answer}</div>
+                    </div>
+                ))}
+            </div>
+        </section>
     );
 }
 
